7d4669b287bb24deb642fb77d8c14a4691ef73b7
Henrique Rodrigues <henrique@hostnet.com.br>
2019-02-24 21:36:21 -0300

    Adicionando busca por nome

--- app/Http/Controllers/PlanetaController.php
+++ app/Http/Controllers/PlanetaController.php
@@ -16,8 +16,23 @@ class PlanetaController extends Controller
         return response()->json(Planeta::All(), 200);
     }
 
+    /**
+     * Busca um planeta pelo ID
+     *
+     * @return void
+     */
     public function searchByID($id)
     {
         return response()->json(Planeta::find($id), 200);
+    }
+
+    /**
+     * Lista um planeta pelo Nome
+     *
+     * @return void
+     */
+    public function searchByName($nome)
+    {
+        return response()->json(Planeta::where('nome', $nome)->first(), 200);
     }
 }
