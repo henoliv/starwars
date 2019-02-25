40def628210bb4dad5ba47d747ab143cb0c9611f
Henrique Rodrigues <henrique@hostnet.com.br>
2019-02-24 21:18:28 -0300

    Criando controller

---
+++ app/Http/Controllers/PlanetaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Planeta;
+
+class PlanetaController extends Controller
+{
+    /**
+     * Lista todos os planetas do banco de dados
+     *
+     * @return void
+     */
+    public function list()
+    {
+        return response()->json(Planeta::All(), 200);
+    }
+
+    public function searchByID($id)
+    {
+        return response()->json(Planeta::find($id), 200);
+    }
+}
